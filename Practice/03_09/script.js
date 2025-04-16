2f2a1048733efd224873a221dc39a50d039b2a98
jdalmeria-tech <alm24008@byui.edu>
2025-04-16 11:52:38 +0800

    reviewed learning methods in JS

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -23,4 +23,37 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  newName: function (updateName) {
+    this.name = updateName;
+  },
+  updateVolume: function (newVolume) {
+    this.volume = newVolume;
+  },
+  newColor: function (newColor) {
+    this.color = newColor;
+  },
+  newPocketNum: function (newValue) {
+    this.pocketNum = newValue;
+  }
 };
+
+console.log("Before... ");
+console.log("Name: ", backpack.name);
+console.log("Volume: ", backpack.volume);
+console.log("Color: ", backpack.color);
+console.log("Number of Pockets: ", backpack.pocketNum);
+// update usind the methods created
+backpack.newName("Legion Gaming Backpack");
+backpack.updateVolume(45);
+backpack.newColor("matte black and gray");
+backpack.newPocketNum(25);
+
+console.log("After... ");
+console.log("Name: ", backpack.name);
+console.log("Volume: ", backpack.volume);
+console.log("Color: ", backpack.color);
+console.log("Number of Pockets: ", backpack.pocketNum);
+
+
+
+

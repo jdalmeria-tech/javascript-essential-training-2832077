e08f0374e7774969e0439f503e8a7655eea7f7b3
jdalmeria-tech <alm24008@byui.edu>
2025-04-16 10:42:24 +0800

    making object exercise for review (:

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,26 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const luggage = {
+  name: "Samsonite",
+  color: "Red",
+  volume: 250,
+  handleLength: 50,
+  backpack: {
+    color: "black and neon blue",
+    gadget: "laptop"
+  },
+  newHandleLength: function (length) {
+    this.handleLength = length;
+  },
+};
+
+// using the bracket [] to access the property inside an object
+let handle = "handleLength";
+console.log("This is the adjustable handle", luggage[handle]);
+
+// dot notation the most common way
+console.log("Inside this luggage is a backpack:", luggage.backpack.color);
+console.log("Inside this backpack is a gaming gadget:", luggage.backpack.gadget)
+
